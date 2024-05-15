b55a8436731201ca5c0a18a68b20b97fcb07ed13
1234567abule <bsc-45-21@unima.ac.mw>
2024-05-15 16:48:39 -0700

    adding my component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
+import React from 'react';
 import './App.css';
 
-function App() {
+import {useState} from "react";
+import cashloan from './components/header/loan/moneyloan/cashloan';
+
+
+
+function Mytouch(){
+const [count, setCount] =useState(0);
+
+function Clicking(){
+  setCount(count+1);
+  }
+return (
+<button onClick={Clicking} >
+Clicked {count} times
+</button>
+);
+}
+
+export default function App(){ 
   return (
     <div className="App">
+      
+      <p><Mytouch /></p>
+      <p><Mytouch /></p>
+      <p><cashloan /></p>
+      
       
     </div>
   );
 }
 
-export default App;
+ 
---
+++ src/components/header/loan/moneyloan/cashloan.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "cashloan.css"
+
+
+
+
+export default function cashloan(){
+return (<div className="me">
+    
+    <div className="naming" >
+<h2> Personal loan</h2>
+<button><h3>more details</h3></button>
+</div>
+
+  <div className="naming" >
+<h2> Input loan</h2>
+<button><h3>more details</h3></button>
+</div>
+
+<div className="naming" >
+<h2> Cash </h2>
+<h3><button>more details</button></h3>
+</div>
+
+</div>
+);
+
+}
