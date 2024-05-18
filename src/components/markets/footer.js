a1507c4c2c7e8a757465b8c1d6a8f1ca20c0d62b
HASSAN ZAMIN <162346010+bsc-44-21@users.noreply.github.com>
2024-05-18 08:29:48 -0700

    Adjusting footer background color to gree and making the logo and my farm text a link so that when the user click, he moves to the top (all done in footer.js)

---
+++ src/components/markets/footer.js
@@ -0,0 +1,91 @@
+import React from "react";
+import logo from "./logo.png";
+const Footer = () => {
+  return (
+    <div className="pt-[7%]">
+      <div className="flex bg-[green] text-[white] pt-[1px] p-10px">
+        <div className="flex-grow: 1">
+          <a href="#" className="pt-[5%] pl-[22%]">
+            {" "}
+            <img
+              src={logo}
+              alt="My App Farm Logo"
+              className="w-[50px] h-auto"
+            />
+            <p className="text-[white] font-[bold]">
+              My-Farm Malawi
+              <br />A Free Society for Malawians
+            </p>
+          </a>
+        </div>
+        <div className="flex-grow: 1 pl-[22%] pt-[4%]">
+          <h3 className="font-[bold]">Quick links</h3>
+          <ul className="list-none">
+            <li>
+              <a
+                href="https://www.admarc.co.mw"
+                className="text-[white] hover:text-customYellow"
+              >
+                Contact us
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.admarc.co.mw"
+                className="text-[white] hover:text-customYellow"
+              >
+                Our team
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.admarc.co.mw"
+                className="text-[white] hover:text-customYellow"
+              >
+                Our Story
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="flex-grow: 1 pl-[28%] pt-[4%]">
+          <h3>Contact Info.</h3>
+          <p>
+            Malawi My Farm,
+            <br />
+            P.O. Box 91,
+            <br />
+            Zomba, Malawi
+          </p>
+          <p>Follow us Now!!!!</p>
+          <div className="Link-icons">
+            <a href="https://www.facebook.com" className="inline-block mr-2.5">
+              <img
+                src="https://img.icons8.com/?size=48&id=13912&format=png"
+                alt="facebook"
+              />
+            </a>
+            <a href="https://www.twitter.com" className="inline-block mr-2.5">
+              <img
+                src="https://img.icons8.com/?size=48&id=13963&format=png"
+                alt="Twitter"
+              />
+            </a>
+            <a href="https://www.instagram.com" className="inline-block mr-2.5">
+              <img
+                src="https://img.icons8.com/?size=48&id=32323&format=png"
+                alt="instagram"
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="bg-[rgb(255,208,0)] pb-[0%] px-[0%] p-[0.1%]">
+        <div className="font-[bold] text-center">
+          <p>©2024 My Farm Malawi. All Rights Reserved.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/components/markets/home.js
+++ src/components/markets/home.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ImageBanner from "./imagebarner";
+import Footer from "./footer";
 
 const Home = () => {
   return (
     <div>
       <ImageBanner />
+      <div>
+        <Footer />
+      </div>
     </div>
   );
 };
